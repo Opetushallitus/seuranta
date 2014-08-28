587603c1a5889f2d0c1c2e81d762e42634ca49ee
jussijartamo <jussi@jartamo@gofore.com>
2014-08-28 15:08:39 +0300

    sijoittelun ajettu merkkaus oli implementoimatta

--- seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/SijoittelunSeurantaResourceImpl.java
+++ seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/SijoittelunSeurantaResourceImpl.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
@@ -17,46 +19,54 @@ import fi.vm.sade.valinta.seuranta.sijoittelu.dto.SijoitteluDto;
 
 @Api(value = "/sijoittelunseuranta", description = "Sijoittelun seurantapalvelun rajapinta")
 @Component
-public class SijoittelunSeurantaResourceImpl implements SijoittelunSeurantaResource {
+public class SijoittelunSeurantaResourceImpl implements
+		SijoittelunSeurantaResource {
 
+	private static final Logger LOG = LoggerFactory
+			.getLogger(SijoittelunSeurantaResourceImpl.class);
 	@Autowired
 	private SijoittelunSeurantaDao sijoittelunSeurantaDao;
-	
+
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "Palauttaa kaikki kantaan tallennetut sijoittelutilat", response = Collection.class)
 	public Collection<SijoitteluDto> hae() {
 		return sijoittelunSeurantaDao.hae();
 	}
-	
+
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "Palauttaa haun sijoittelun tilan", response = Collection.class)
 	public SijoitteluDto hae(String hakuOid) {
 		return sijoittelunSeurantaDao.hae(hakuOid);
 	}
-	
+
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "Merkkaa sijoittelun ajetuksi haulle", response = Collection.class)
 	public SijoitteluDto merkkaaSijoittelunAjetuksi(String hakuOid) {
-		return null;
+		return sijoittelunSeurantaDao.paivitaViimeksiAjettuPaivamaara(hakuOid);
 	}
-	
+
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "Asettaa jatkuvan sijoittelun haulle", response = Collection.class)
-	public SijoitteluDto merkkaaSijoittelunAjossaTila(String hakuOid, boolean tila) {
+	public SijoitteluDto merkkaaSijoittelunAjossaTila(String hakuOid,
+			boolean tila) {
 		return sijoittelunSeurantaDao.asetaJatkuvaSijoittelu(hakuOid, tila);
 	}
 
-    @PreAuthorize("isAuthenticated()")
-    @ApiOperation(value = "Asettaa jatkuvan sijoittelun haulle", response = Collection.class)
-    public SijoitteluDto merkkaaSijoittelunAjossaVirhe(String hakuOid, String virhe) {
-        return null;
-    }
+	@PreAuthorize("isAuthenticated()")
+	@ApiOperation(value = "Asettaa jatkuvan sijoittelun haulle", response = Collection.class)
+	public SijoitteluDto merkkaaSijoittelunAjossaVirhe(String hakuOid,
+			String virhe) {
+		LOG.error("Sijoittelun ajossa on virhe metodia ei ole implementoitu!");
+		throw new RuntimeException("Yritettiin merkata haulle " + hakuOid
+				+ " virhe " + virhe
+				+ " mutta virheen merkkausta ei ole implementoitu seurantaan!");
+	}
 
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "Poistaa hakuun liittyvat sijoittelutilat kannasta", response = Collection.class)
 	public Response poistaSijoittelu(String hakuOid) {
-        sijoittelunSeurantaDao.poistaSijoittelu(hakuOid);
-        return Response.ok().build();
+		sijoittelunSeurantaDao.poistaSijoittelu(hakuOid);
+		return Response.ok().build();
 	}
-	
+
 }
