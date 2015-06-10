2de58d058b903dfca78fd92fd64c42aa49b88791
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 22:22:26 +0300

    reformat and remove some javadoc

--- seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/SijoittelunSeurantaResourceImpl.java
+++ seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/SijoittelunSeurantaResourceImpl.java
@@ -20,53 +20,48 @@ import fi.vm.sade.valinta.seuranta.sijoittelu.dto.SijoitteluDto;
 
 @Api(value = "/sijoittelunseuranta", description = "Sijoittelun seurantapalvelun rajapinta")
 @Component
-public class SijoittelunSeurantaResourceImpl implements
-		SijoittelunSeurantaResource {
+public class SijoittelunSeurantaResourceImpl implements SijoittelunSeurantaResource {
+    private static final Logger LOG = LoggerFactory.getLogger(SijoittelunSeurantaResourceImpl.class);
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(SijoittelunSeurantaResourceImpl.class);
-	@Autowired
-	private SijoittelunSeurantaDao sijoittelunSeurantaDao;
+    @Autowired
+    private SijoittelunSeurantaDao sijoittelunSeurantaDao;
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Palauttaa kaikki kantaan tallennetut sijoittelutilat", response = Collection.class)
-	public Collection<SijoitteluDto> hae() {
-		return sijoittelunSeurantaDao.hae();
-	}
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Palauttaa kaikki kantaan tallennetut sijoittelutilat", response = Collection.class)
+    public Collection<SijoitteluDto> hae() {
+        return sijoittelunSeurantaDao.hae();
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Palauttaa haun sijoittelun tilan", response = Collection.class)
-	public SijoitteluDto hae(String hakuOid) {
-		return sijoittelunSeurantaDao.hae(hakuOid);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Palauttaa haun sijoittelun tilan", response = Collection.class)
+    public SijoitteluDto hae(String hakuOid) {
+        return sijoittelunSeurantaDao.hae(hakuOid);
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Merkkaa sijoittelun ajetuksi haulle", response = Collection.class)
-	public SijoitteluDto merkkaaSijoittelunAjetuksi(String hakuOid) {
-		return sijoittelunSeurantaDao.paivitaViimeksiAjettuPaivamaara(hakuOid);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Merkkaa sijoittelun ajetuksi haulle", response = Collection.class)
+    public SijoitteluDto merkkaaSijoittelunAjetuksi(String hakuOid) {
+        return sijoittelunSeurantaDao.paivitaViimeksiAjettuPaivamaara(hakuOid);
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Asettaa jatkuvan sijoittelun haulle", response = Collection.class)
-	public SijoitteluDto merkkaaSijoittelunAjossaTila(String hakuOid,
-			boolean tila) {
-		return sijoittelunSeurantaDao.asetaJatkuvaSijoittelu(hakuOid, tila);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Asettaa jatkuvan sijoittelun haulle", response = Collection.class)
+    public SijoitteluDto merkkaaSijoittelunAjossaTila(String hakuOid, boolean tila) {
+        return sijoittelunSeurantaDao.asetaJatkuvaSijoittelu(hakuOid, tila);
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Asettaa jatkuvan sijoittelulle virheen", response = Collection.class)
-	public SijoitteluDto merkkaaSijoittelunAjossaVirhe(String hakuOid,
-			String virhe) {
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Asettaa jatkuvan sijoittelulle virheen", response = Collection.class)
+    public SijoitteluDto merkkaaSijoittelunAjossaVirhe(String hakuOid, String virhe) {
         return sijoittelunSeurantaDao.asetaSijoitteluVirhe(hakuOid, virhe);
-	}
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@ApiOperation(value = "Poistaa hakuun liittyvat sijoittelutilat kannasta", response = Collection.class)
-	public Response poistaSijoittelu(String hakuOid) {
-		sijoittelunSeurantaDao.poistaSijoittelu(hakuOid);
-		return Response.ok().build();
-	}
-
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation(value = "Poistaa hakuun liittyvat sijoittelutilat kannasta", response = Collection.class)
+    public Response poistaSijoittelu(String hakuOid) {
+        sijoittelunSeurantaDao.poistaSijoittelu(hakuOid);
+        return Response.ok().build();
+    }
 
     @PreAuthorize("isAuthenticated()")
     @ApiOperation(value = "Päivittää sijoittelun aloitusajankohdan", response = Response.class)
