ab07a9bd95405ef225ff460f93318fd86ed77171
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 22:21:44 +0300

    reformat and remove some javadoc

--- seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/ResourceConfiguration.java
+++ seuranta-service/src/main/java/fi/vm/sade/valinta/seuranta/resource/impl/ResourceConfiguration.java
@@ -10,41 +10,24 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-/**
- * 
- * @author Jussi Jartamo
- * 
- */
 public class ResourceConfiguration extends ResourceConfig {
-	//
-	public ResourceConfiguration() {
-		// packages("fi.vm.sade.valinta.dokumenttipalvelu.resource.impl.DokumenttiResourceImpl");
-		// json output and input
-		register(new ContainerResponseFilter() {
-
-			@Override
-			public void filter(ContainerRequestContext requestContext,
-					ContainerResponseContext responseContext)
-					throws IOException {
-				responseContext.getHeaders().add("Access-Control-Allow-Origin",
-						"*");
-			}
-		});
-		register(SseFeature.class);
-		register(JacksonFeature.class);
-		// register(MultiPartFeature.class);
-
-		register(DokumentinSeurantaResourceImpl.class);
-		register(LaskennanSeurantaResourceImpl.class);
-		register(SijoittelunSeurantaResourceImpl.class);
+    public ResourceConfiguration() {
+        // packages("fi.vm.sade.valinta.dokumenttipalvelu.resource.impl.DokumenttiResourceImpl");
+        register(new ContainerResponseFilter() {
+            @Override
+            public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+                responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+            }
+        });
+        register(SseFeature.class);
+        register(JacksonFeature.class);
+        register(DokumentinSeurantaResourceImpl.class);
+        register(LaskennanSeurantaResourceImpl.class);
+        register(SijoittelunSeurantaResourceImpl.class);
         register(SessionResourceImpl.class);
-		/**
-		 * CORS Filter
-		 */
-
-		registerInstances(
-				new com.wordnik.swagger.jaxrs.listing.ResourceListingProvider(),
-				new com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider());
-		register(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
-	}
+        registerInstances(
+                new com.wordnik.swagger.jaxrs.listing.ResourceListingProvider(),
+                new com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider());
+        register(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+    }
 }
